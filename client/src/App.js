6c17763adcc77698ad4973617cd030f3831820a5
jasonk <jasonkhalili@gmail.com>
2021-12-23 23:01:12 -0800

    Working GET request to backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 const App = () => {
+
+  useEffect(() => {
+    axios.get('http://localhost:3200/events?_page=1')
+      .then(res => {
+        console.log(res.data);
+      })
+  })
+
   return (
     <h1>It works</h1>
   );
