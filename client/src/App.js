06f43908e7fb66ee9c65a1f294f179803d109e05
jasonk <jasonkhalili@gmail.com>
2021-12-24 20:42:29 -0800

    Added pagination

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { TextField } from '@mui/material';
+import { TextField, Pagination } from '@mui/material';
 import axios from 'axios';
 
 import List from './List';
@@ -7,14 +7,15 @@ import List from './List';
 const App = () => {
 
   const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:3200/events?q=${query}&_page=1`)
+    axios.get(`http://localhost:3200/events?q=${query}&_page=${page}`)
       .then(res => {
         setEvents(res.data);
       })
-  }, [query])
+  }, [query, page])
 
   return (
     <>
@@ -25,6 +26,7 @@ const App = () => {
       onChange={(e) => setQuery(e.target.value)}
       />
       <List events={events} />
+      <Pagination count={10} onChange={(event, page) => setPage(page)} />
     </>
   )
 }
