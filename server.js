a993979e188b0744d2b3158c403c0b9708c1f13f
jasonk <jasonkhalili@gmail.com>
2021-12-27 10:08:08 -0800

    Removed json-server middleware

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
-server.use(middlewares);
 server.use(router);
 
 server.listen(port, () => console.log(`Listening on port: ${port}`));
